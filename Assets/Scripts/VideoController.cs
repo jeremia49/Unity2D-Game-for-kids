33deff768fab1f0be7bf011bef1df13e2a04774a
Jeremia <39850621+jeremia49@users.noreply.github.com>
2021-04-24 16:06:44 +0700

    mudah-mudahan yang paling terakhir

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -8,6 +8,11 @@ public class VideoController : MonoBehaviour
     public UnityEngine.Video.VideoPlayer videoplayer;
     private Text text;
 
+    private void Awake()
+    {
+        videoplayer.Prepare();
+    }
+
     private void Start()
     {
         text = this.GetComponentInChildren<UnityEngine.UI.Text>();
@@ -25,5 +30,29 @@ public class VideoController : MonoBehaviour
             text.text = "Putar";
         }
     }
+    public void forward()
+    {
+        if (videoplayer.clip.length < videoplayer.time - 10f)
+        {
+            videoplayer.time = videoplayer.clip.length - 10f;
+        }
+        else
+        {
+            videoplayer.time += 10f;
+        }
+    }
+
+    public void backward()
+    {
+        if (videoplayer.time < 10) {
+            videoplayer.time = 0f;
+        }
+        else
+        {
+            videoplayer.time -= 10f;
+        }
+    }
+
+
 
 }
